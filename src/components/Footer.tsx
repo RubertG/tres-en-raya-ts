f47eb3e4a3f2a54f7ef9d4e909527d7414c33414
Rubert_G <rubertgonzalez5@gmail.com>
2023-07-04 14:29:18 -0500

    CORRECCION: Footer cambio de color de turno

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,15 +7,17 @@ interface Props {
 }
 
 const Footer: React.FC<Props> = ({ turn, resetBoard }): JSX.Element => {
-  const className = 'text-white text-3xl py-3 px-5 bg-slate-700 rounded-md transition-colors'
-  const classNameX = `${className} ${turn === OPTIONS.X ? 'bg-red-500' : ''}`
-  const classNameY = `${className} ${turn === OPTIONS.O ? 'bg-blue-500' : ''}`
+  const className = 'text-white text-3xl py-3 px-5 rounded-md transition-colors'
 
   return (
     <footer className='flex justify-center items-center gap-8 mt-7'>
       <div className='flex justify-center items-center gap-3'>
-        <p className={classNameX}>{OPTIONS.X}</p>
-        <p className={classNameY}>{OPTIONS.O}</p>
+        <p
+          className={`${className} ${turn === OPTIONS.X ? 'bg-red-500' : 'bg-slate-700'}`}
+        >{OPTIONS.X}</p>
+        <p
+          className={`${className} ${turn === OPTIONS.O ? 'bg-blue-500' : 'bg-slate-700'}`}
+        >{OPTIONS.O}</p>
       </div>
       <button
         className="block bg-slate-400 font-bold py-1 px-4 rounded-md text-lg text-slate-950 md:hover:bg-slate-500 transition-colors"
